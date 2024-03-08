d4087dc16d0960065be7f0abf84911175e0d17ca
sunbeam <yogeshkasar4898@gmail.com>
2024-03-08 14:04:31 +0530

    Question 4

---
+++ Assignment1/Q4.py
@@ -0,0 +1,11 @@
+def avg():
+    num1 = int(input("Enter marks of num1: "))
+    num2= int(input("Enter marks of num2: "))
+    num3= int(input("Enter marks of num3: "))
+    average = (num1+num2+num3)//3
+    print(f"The avg of three number ={average}")
+
+
+
+
+avg()
